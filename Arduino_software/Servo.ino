5d4c23eaf47d9db3a6ca5695ade553d99129c9bb
Nicholas Lislien Baekken <nicholb@live.com>
2020-09-03 11:09:24 +0200

    Added potmeter to controll PWM signal

--- Arduino_software/Servo.ino
+++ Arduino_software/Servo.ino
@@ -1,18 +1,31 @@
 #include <servo.h>
 
-int servo1Pin = 3
-int servo2Pin = 4
+const int servo1Pin = 3
+const int servo2Pin = 4
+
+const int potmeter = A0;
 
 Servo Servo1;
 Servo Servo2;
 
-void setup() {
+void setup() 
+{    
     Servo1.attach(servo1Pin);
     Servo2.attach(servo2Pin);
     Servo1.writeMicroseconds(1100);
-    Servo2.writeMicroseconds(1100)
+    Servo2.writeMicroseconds(1100);
+    
+    Serial.begin(115200);
 }
 
-void loop() {
+void loop() 
+{
+    int speedValue = analogRead(potmeter);
 
+    speedValue = map(speedValue, 0, 1000, 1000, 2000)
+
+    Serial.print(speedValue)
+
+    Servo1.writeMicroseconds(speedValue);
+    Servo2.writeMicroseconds(speedValue);
 }
