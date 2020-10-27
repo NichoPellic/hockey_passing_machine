3b8a848610c7b74e59b28da02344d15c89437842
Nicholas Lislien Baekken <nicholb@live.com>
2020-10-27 20:36:15 +0100

    Cleaned up the code

--- Image_processing_python/yolo_testing.py
+++ Image_processing_python/yolo_testing.py
@@ -1,50 +1,78 @@
-#Puck buddy v0.1
+#!/usr/bin/env python3
+
+#Puck buddy v1.0
 
 import cv2
 import numpy as np
-import send_data
+import SerialDevice
 import support_functions
 
-#Add movement threashold to avoid jitter in the x values
-global arduino 
-arduino = send_data.ConnectDevice(send_data.GetSerialPorts())
+#Global variables
+headlessMode = False
+yoloTiny = True
+enableCuda = True
+controllerConnected = False
+
+confidenceThreshold = 0.5
+nmsThreshold = 0.3
 lastxValue = 0
 videoHeight = 720
 videoWidht = 1280
-xValueDeadzone = 10 #Should probably be proportinal to video input width
+xValueDeadzone = 10 #Allows the target to move without updating coordinate values
+
+#Connect to the controller
+arduino = SerialDevice.Arduino()
+
+SerialDevice.GetSerialPorts()
+
+if(arduino.ConnectDevice()):
+    controllerConnected = True
+else:
+    print("Program running with reduced functionallity!")
 
 
+#Prepare object detection
 classesFile = r'C:\Users\Nicholas\source\repos\hockey\Image_processing_python\coco_classes.txt'
 classNames = []
 
 with open(classesFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 
-#Un-comment the one you want to use 
-#Yolo 320 for better recognition and Yolov3 Tiny for better performance
-
 #NB!
 #Rember to update the path values
+if(not yoloTiny):
+    #Yolo 320
+    modelConfiguration = r"C:\Users\Nicholas\source\repos\hockey\Image_processing_python\yolov3.cfg"
+    modelWeights = r"C:\Users\Nicholas\source\repos\hockey\Image_processing_python\yolov3.weights"
 
-#Yolo 320
-modelConfiguration = r"C:\Users\Nicholas\source\repos\hockey\Image_processing_python\yolov3.cfg"
-modelWeights = r"C:\Users\Nicholas\source\repos\hockey\Image_processing_python\yolov3.weights"
-
-#Yolo3 Tiny
-#modelConfiguration = r"C:\Users\Nicholas\source\repos\hockey\Image_processing_python\yolov3-tiny.cfg"
-#modelWeights = r"C:\Users\Nicholas\source\repos\hockey\Image_processing_python\yolov3-tiny.weights"
+else:
+    #Yolo3 Tiny
+    modelConfiguration = r"C:\Users\Nicholas\source\repos\hockey\Image_processing_python\yolov3-tiny.cfg"
+    modelWeights = r"C:\Users\Nicholas\source\repos\hockey\Image_processing_python\yolov3-tiny.weights"
 
 
 net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
-confidenceThreshold = 0.8
-nmsThreshold = 0.3
+#Uses CUDA if Nvidia card is present and OpenCV is compiled for it
+try:
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+except:
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
-cap = cv2.VideoCapture(1)
-cap.set(3, videoWidht)
-cap.set(4, videoHeight)
+#Add this into a loop as the program will not function without a camera
+try:
+    #Always defaults to the first camera
+    cap = cv2.VideoCapture(0)
+except:
+    print("Unable to connect to camera")
+
+if(not headlessMode):
+    cap.set(3, videoWidht)
+    cap.set(4, videoHeight)
+
+
 whT = 320
 
 def findObjects(outputs, img, lastxValue):
@@ -79,24 +107,28 @@ def findObjects(outputs, img, lastxValue):
 
     #Checks if the object has moved enough to update the control data sent to the stepper
     #Add check to see if the target is a person
-    #Need to convert the xValue into degrees
-    if(arduino is not None):
+    #Need to convert qthe xValue into degrees
+    if(controllerConnected):
         if(((lastxValue - xValueDeadzone) > xValue) or ((lastxValue + xValueDeadzone) < xValue)):
             if(xValue != 0):
-                send_data.SendData(arduino, support_functions.MapValue(xValue, 0, videoWidht, 0, 100))
-                #print(send_data.ReadData(arduino))
+                arduino.SendData(support_functions.MapValue(xValue, 0, videoWidht, 0, 180))
                 lastxValue = xValue   
-        
 
-    indicies = cv2.dnn.NMSBoxes(bbox, confValues, confidenceThreshold, nmsThreshold)
+        if(arduino.ReadData() != ""):
+            print(arduino.ReadData())
 
-    for i in indicies:
-        i = i[0]
-        box = bbox[i]
-        x,y,w,h = box[0], box[1], box[2], box[3]  
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 255), 2)
-        cv2.putText(img, f'{classNames[classIds[i]].upper()} {int(confValues[i]*100)}%',
-                           (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255))        
+
+    if(not headlessMode):
+        indicies = cv2.dnn.NMSBoxes(bbox, confValues, confidenceThreshold, nmsThreshold)
+
+        for i in indicies:
+            i = i[0]
+            box = bbox[i]
+            x,y,w,h = box[0], box[1], box[2], box[3]  
+            cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 255), 2)
+            cv2.putText(img, f'{classNames[classIds[i]].upper()} {int(confValues[i]*100)}%',
+                            (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255))        
+
     return xValue
 
 
@@ -120,11 +152,13 @@ def main():
         lastxValue = findObjects(outputs, img, lastxValue)
 
         fps = cv2.getTickFrequency()/(cv2.getTickCount() - timer)
-        cv2.putText(img, str(int(fps)), (75, 50), cv2.FONT_ITALIC, 0.7, (0,0,255), 2)
-        cv2.imshow('Image', img)
 
-        if(cv2.waitKey(1) & 0xff == ord('q')):
-            break
+        if(not headlessMode):
+            cv2.putText(img, str(int(fps)), (75, 50), cv2.FONT_ITALIC, 0.7, (0,0,255), 2)
+            cv2.imshow('Image', img)
+
+            if(cv2.waitKey(1) & 0xff == ord('q')):
+                break
 
 #Program to run
 main()
